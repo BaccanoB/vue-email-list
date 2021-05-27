502fe4225975664675c557c687f848a34e4e7e86
BaccanoB <beatrice.baccano@yahoo.it>
2021-05-27 16:31:50 +0200

    terminato esercizio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var app = new Vue (
+    {
+        el:"#root",
+        data: {
+            mail:[],
+        },
+        mounted: function(){
+            for(var i = 0; i < 10; i++){
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then(
+                        (result) => {
+                            this.mail.push(result.data.response);
+                            }
+                           
+                        
+                    )
+            }        
+        }
+    }
+)
